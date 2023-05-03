57d0afb64ca0f78cafdda537ba0c763a078ffd9d
KatiFerrari <katielidasilvaferrari26@gmail.com>
2023-05-03 13:09:43 -0400

    commit inicial

---
+++ script.js
@@ -0,0 +1,23 @@
+// divListaProduto.insertAdjacentHTML('afterbegin', '');
+class Produto{
+    constructor(nome,dataCad, descricao, preco){
+        this.nome = nome;
+        this.dataCad = dataCad;
+        this.descricao = descricao;
+        this.preco = preco;
+    }
+
+    mostrarProdutos(){
+        return this.nome + " " + this.dataCad + " " +  this.descricao + " " + this.preco
+    }
+
+}
+class DestProdutos extends Produto {
+    constructor(nome,dataCad, descricao, preco){
+        super (nome,dataCad, descricao, preco)
+    }
+}
+
+const produtos = new Produto("Caderno", "26/02/2023", "Caderno Universitário Fases da Lua 160 folhas", 55.5)
+const DestProdutos = new Destaque("Caderno", "26/02/2023", "Caderno Universitário Fases da Lua 160 folhas", 55.5)
+console.log(produto.mostrarProdutos())
